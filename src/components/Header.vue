b5c13f99773ed5da69feba05061335d47262eec9
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2024-11-06 00:13:14 +0100

    update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -39,8 +39,10 @@ const toggleMenu = () => {
         >Contact</a>
       </div>
       <div class="flex flex-1 items-center justify-end gap-x-6">
-        <a href="#"
-          class="rounded-md bg-accent px-3 py-2 text-base font-semibold text-secondary shadow-sm hover:bg-accent-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Je m'inscris</a>
+        <router-link to="/adhesion"
+          class="rounded-md bg-accent px-3 py-2 text-base font-semibold text-secondary shadow-sm hover:bg-accent-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          Je m'inscris
+        </router-link>
       </div>
       <div class="flex lg:hidden">
         <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
@@ -60,12 +62,14 @@ const toggleMenu = () => {
       <div
         class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-primary px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
         <div class="flex items-center gap-x-6">
-          <a href="#" class="-m-1.5 p-1.5">
+          <router-link to="/" class="-m-1.5 p-1.5">
             <span class="sr-only">Toul'Barz</span>
             <img class="h-28 w-auto" src="/img/logo.webp" alt="Logo Toul'Barz">
-          </a>
-          <a href="#"
-            class="ml-auto rounded-md bg-accent px-3 py-2 text-base font-semibold text-secondary shadow-sm hover:bg-accent-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Je m'inscris</a>
+          </router-link>
+          <router-link to="/adhesion"
+            class="ml-auto rounded-md bg-accent px-3 py-2 text-base font-semibold text-secondary shadow-sm hover:bg-accent-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Je m'inscris
+          </router-link>
           <button @click="toggleMenu" type="button" class="-m-2.5 rounded-md p-2.5 text-secondary">
             <span class="sr-only">Fermer le menu</span>
             <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
@@ -77,14 +81,14 @@ const toggleMenu = () => {
         <div class="mt-6 flow-root">
           <div class="-my-6 divide-y divide-gray-500/10">
             <div class="space-y-2 py-6">
-              <a href="#decouverte"
-                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-secondary hover:bg-accent duration-200">Accueil</a>
-              <a href="#evenements"
-                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-secondary hover:bg-accent duration-200">Événements</a>
-              <a href="#adhesion"
-                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-secondary hover:bg-accent duration-200">Adhésion</a>
-              <a href="#contact"
-                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-secondary hover:bg-accent duration-200">Contact</a>
+              <router-link to="/"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-secondary hover:bg-accent duration-200">Accueil</router-link>
+              <router-link to="/"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-secondary hover:bg-accent duration-200">Événements</router-link>
+              <router-link to="/adhesion"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-secondary hover:bg-accent duration-200">Adhésion</router-link>
+              <router-link to="/"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-secondary hover:bg-accent duration-200">Contact</router-link>
             </div>
           </div>
         </div>
