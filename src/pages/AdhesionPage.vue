082aa644d686e0627817c29dc5cca8d688cb0f91
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2024-11-05 15:58:26 +0100

    Update adhesion

--- src/pages/AdhesionPage.vue
+++ src/pages/AdhesionPage.vue
@@ -1,12 +1,9 @@
 <template>
-
-
-<iframe id="haWidget" allowtransparency="true" scrolling="auto" src="https://www.helloasso.com/associations/toulbarz/adhesions/adhesions-toulbarz-2024-2025/widget" style="width: 100%; height: 750px;"></iframe>
-
-
+    <iframe 
+        id="haWidget"
+        allowtransparency="true"
+        scrolling="auto" 
+        src="https://www.helloasso.com/associations/toulbarz/adhesions/adhesions-toulbarz-2024-2025/widget"
+        style="width: 100%; height: 750px;">
+    </iframe>
 </template>
-
-
-<script setup>
-
-</script>
