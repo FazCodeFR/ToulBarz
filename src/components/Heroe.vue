12cb4760e0281b7abc17e8e553f5e0967dd4677c
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2024-12-09 17:22:33 +0100

    Fix heroe

--- src/components/Heroe.vue
+++ src/components/Heroe.vue
@@ -1,40 +1,41 @@
 <template>
-  <!-- heroe -->
-  <header class="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20">
-    <div
-      class="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
-      aria-hidden="true"></div>
-    <div class="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
-      <div
-        class="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
-        <h1 class="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
-          Toul'Barz - Transformez votre façon de vous entraîner.
-        </h1>
-        <div class="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
-          <p class="text-lg leading-8 text-gray-600">
-            Rejoignez notre communauté pour des séances de street workout, des événements passionnants et une expérience
-            unique à Toulouse.
-            Développez votre force et connectez-vous avec d'autres passionnés !
+  <div class="bg-gray-900">
+    <div class="relative isolate overflow-hidden h-[100vh]">
+      <img src="/img/home_heroe.png" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover" />
+      <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+        <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+      </div>
+      <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:py-32">
+        <div class="flex flex-col items-center text-center">
+          <img class="h-80 w-auto" src="/img/logo.webp" alt="Logo Toul'Barz">
+          <h1 class="text-3xl font-bold tracking-tight text-white sm:text-5xl">
+            TOUL'BARZ
+          </h1>
+          <p class="mt-4 text-lg leading-8 text-gray-100 font-serif">
+            Quand la force devient une œuvre 
           </p>
-          <div class="mt-10 flex items-center gap-x-6">
-            <router-link to="/adhesion"
-              class="rounded-md bg-accent px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-accent-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent-dark">
-              Je m'inscris
-            </router-link>
-            <a href="#evenements" class="text-sm font-semibold leading-6 text-gray-900">
-              Événements
-              <span aria-hidden="true">→</span>
+
+          <a href="#"
+            class="mt-4 group relative inline-flex items-center justify-center rounded-full border-2 border-white p-3 text-white transition-all duration-200 hover:bg-white hover:text-black disabled:bg-gray-300 disabled:text-gray-500"
+            aria-label="Je m'inscris">
+            <span class="text-base font-light tracking-wide">Rejoins-nous</span>
+          </a>
+
+          <div class="animate-bounce mt-12 flex items-center justify-center gap-x-6">
+            <a href="#" class="text-6xl font-semibold leading-6 text-white">
+              <span aria-hidden="true">↓</span>
             </a>
           </div>
         </div>
-        <img
-          src="/img/missions/mission_decouverte.jpg"
-          alt="Séance de street workout"
-          class="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36">
+      </div>
+      <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+        <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
       </div>
     </div>
-    <div class="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32"></div>
-  </header>
-
-
+  </div>
 </template>
+
+
+<script setup>
+import { ref } from 'vue'
+</script>
