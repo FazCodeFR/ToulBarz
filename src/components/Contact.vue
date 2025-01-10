6c53c2baeab3e719a93853ab10dc40b183d891bb
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2025-01-10 12:04:00 +0100

    UPDATE

---
+++ src/components/Contact.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="bg-white pb-10 sm:py-32">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <h2 class="mt-2 text-4xl font-bold tracking-tight underline underline-offset-4 decoration-accent text-gray-900 sm:text-5xl text-center">
+            Contact
+        </h2>
+        <p class="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
+            Rejoins-nous et découvre notre communauté en pleine action !
+        </p>
+        <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base leading-7 sm:grid-cols-2 sm:gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          <div>
+            <h3 class="border-l border-accent pl-6 font-semibold text-gray-900">Sur un parc</h3>
+            <p class="border-l border-gray-200 pl-6 pt-2 text-gray-600">
+              On s'entraîne régulièrement sur les parcs de notre région. Nos sessions d'entraînements sont annoncées sur nos réseaux sociaux !
+            </p>
+          </div>
+          <div>
+            <h3 class="border-l border-accent pl-6 font-semibold text-gray-900">Sur un évènement</h3>
+            <p class="border-l border-gray-200 pl-6 pt-2 text-gray-600">
+              Je t'invite à nous suivre afin d'être informé des prochains événements à venir !
+            </p>
+          </div>
+          <div>
+            <h3 class="border-l border-accent pl-6 font-semibold text-gray-900">Sur les réseaux</h3>
+            <p class="border-l border-gray-200 pl-6 pt-2 text-gray-600">
+              Rejoins notre communauté et partage tes performances avec nous en nous identifiant sur les réseaux sociaux.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
