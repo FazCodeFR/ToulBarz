6328ed2a62e70385b47b6986b0f2173e37c65b9b
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2025-01-25 15:36:47 +0100

    fix(event): enhance event section layout with improved styling and background effects

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="bg-white py-2 sm:py-24">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl lg:max-w-4xl">
-
-        <!-- Titre principal -->
-        <h1 class="text-center text-3xl font-bold tracking-tight underline underline-offset-4 decoration-accent text-gray-900 sm:text-4xl lg:text-5xl">
-          Événements de l'association
-        </h1>
-
-        <!-- Introduction -->
-        <h2 class="mt-16 mb-8 text-lg font-medium tracking-tight text-gray-800 sm:text-xl">
-          Découvrez nos prochains événements :
-        </h2>
+      <div class="relative isolate bg-white px-6 py-12 sm:py-24 lg:px-8">
+    <div class="absolute inset-x-0 -top-3 transform-gpu overflow-hidden px-36 blur-3xl" aria-hidden="true">
+      <div class="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+    </div>
+    <div class="mx-auto max-w-7xl text-center sm:max-w-4xl">
+      <h1 class="text-3xl font-bold tracking-tight text-accent sm:text-4xl lg:text-5xl underline underline-offset-4 decoration-accent text-gray-900">Événements de l'association</h1>
+      <h2 class="mt-12 mb-6 text-lg font-medium tracking-tight text-gray-800 sm:text-xl">Découvrez nos prochains événements :</h2>
+    </div>
 
         <!-- Tab -->
         <div>
