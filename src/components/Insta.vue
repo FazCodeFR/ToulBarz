335b3e11d4d9b96719ed456907b6a3273acb8f77
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2025-03-14 13:59:27 +0100

    feat(Insta): adjust Instagram header spacing and comment out promotional text

--- src/components/Insta.vue
+++ src/components/Insta.vue
@@ -27,13 +27,13 @@ document.addEventListener("DOMContentLoaded", function () {
 <template>
     <div class="bg-white">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
-        <h2 class="mt-2 text-4xl font-bold tracking-tight underline underline-offset-4 decoration-accent text-gray-900 sm:text-5xl text-center">
+        <h2 class="pb-10 mt-2 text-4xl font-bold tracking-tight underline underline-offset-4 decoration-accent text-gray-900 sm:text-5xl text-center">
             Instagram
         </h2>
-        <p class="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
+        <!-- <p class="pb-10  mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
             Retrouve-nous sur Instagram et partage tes performances avec nous !
-        </p>
-      </div>
+        </p> -->
         <div class="elfsight-app-633bb5bb-c30f-488d-9697-1777a15ae963" data-elfsight-app-lazy></div>
+      </div>
     </div>
 </template>
