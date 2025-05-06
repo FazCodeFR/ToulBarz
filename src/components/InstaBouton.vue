649016b3c88928143a692ee5e648d62697d228f9
FazCodeFR <30906528+FazCodeFR@users.noreply.github.com>
2025-05-06 13:07:26 +0200

    Add tiktok bouton

--- src/components/InstaBouton.vue
+++ src/components/InstaBouton.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
     <a
       href="https://www.instagram.com/toulbarz_tlb/"
       target="_blank"
@@ -7,5 +8,14 @@
     >
       <i class="i-mdi-instagram text-3xl text-pink-500"></i>
     </a>
-  </template>
-  
+
+    <a
+      href="https://www.tiktok.com/@toul_barz"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="fixed left-0 bottom-28 z-50 bg-white shadow-md rounded-r-2xl p-2 transition-all hover:left-2"
+    >
+      <i class="i-bxl-tiktok text-3xl text-black"></i>
+    </a>
+  </div>
+</template>
